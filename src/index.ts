38d7ad002c8af0c8a46ccf6ecde914fc41cb57b2
José Lugo <chepe.time@gmail.com>
2020-08-10 00:09:50 -0500

    Added GraphQL Server

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,13 @@
 import express from "express";
-import bodyParser from "body-parser";
-import { listings } from "./listings";
+import { ApolloServer } from "apollo-server-express";
+
+import { typeDefs, resolvers } from "./graphql";
 
 const app = express();
 const port = 9000;
 
-app.use(bodyParser.json());
-
-app.get("/listings", (_req, res) => res.send(listings));
-
-app.get("/listings/:id", (req, res) => {
-  const id: string = req.params.id;
-  const singleListing = listings.find((el) => el.id === id) || {};
-
-  return res.send(singleListing);
-});
-
-app.delete("/listings/:id", (req, res) => {
-  const id: string = req.params.id;
-
-  for (let i = 0; i < listings.length; i++) {
-    if (listings[i].id === id) {
-      return res.send(listings.splice(i, 1)[0]);
-    }
-  }
-
-  return res.send({});
-});
+const server = new ApolloServer({ typeDefs, resolvers });
+server.applyMiddleware({ app, path: "/api" });
 
 app.listen(port);
 
